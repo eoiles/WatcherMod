d1b45c5f6a054e7c452b20bbaa3e5587a160600c
lamali <l.liebhart@tu-braunschweig.de>
2026-04-12 08:43:26 +0200

    Remove `ArchaicToothPatch`, adjust `Eruption` class for `Transcendence` support, and update relic image config.

--- Code/Cards/Basic/Eruption.cs
+++ Code/Cards/Basic/Eruption.cs
@@ -1,7 +1,10 @@
-﻿using BaseLib.Utils;
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
 using Watcher.Code.Abstract;
+using Watcher.Code.Cards.Ancient;
 using Watcher.Code.Character;
 using Watcher.Code.Commands;
 using Watcher.Code.Stances;
@@ -9,7 +12,7 @@ using Watcher.Code.Stances;
 namespace Watcher.Code.Cards.Basic;
 
 [Pool(typeof(WatcherCardPool))]
-public sealed class Eruption : WatcherCardModel
+public sealed class Eruption : WatcherCardModel, ITranscendenceCard
 {
     public Eruption() : base(2, CardType.Attack, CardRarity.Basic, TargetType.AnyEnemy)
     {
@@ -24,5 +27,10 @@ public sealed class Eruption : WatcherCardModel
         await CommonActions.CardAttack(this, cardPlay).WithHitFx("vfx/vfx_attack_slash")
             .Execute(ctx);
         await StanceCmd.EnterWrath(ctx, Owner, cardPlay.Card);
+    }
+
+    public CardModel GetTranscendenceTransformedCard()
+    {
+        return ModelDb.Card<AncientCard2>();
     }
 }
